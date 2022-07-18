8d8c439677dcb2bafe1cca5f62a8bbc9a4c8a1f4
joao-cigana <joaovitorcigana@gmail.com>
2022-07-18 20:20:18 -0300

    project created

---
+++ src/test/java/com/joaocigana/springproject/SpringprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaocigana.springproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
