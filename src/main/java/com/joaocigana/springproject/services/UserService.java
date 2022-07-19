4978064831ee2b0f4a0c7ba0a84b1775ea103fb8
joao-cigana <joaovitorcigana@gmail.com>
2022-07-18 21:51:33 -0300

    Service layer, component registration

---
+++ src/main/java/com/joaocigana/springproject/services/UserService.java
@@ -0,0 +1,26 @@
+package com.joaocigana.springproject.services;
+
+import com.joaocigana.springproject.entities.User;
+import com.joaocigana.springproject.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
