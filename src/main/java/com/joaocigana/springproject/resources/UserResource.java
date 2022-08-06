a9c5260dc638db6602bf40fb4b74b1e506b3ae11
joao-cigana <joaovitorcigana@gmail.com>
2022-08-06 14:44:34 -0300

    user insert

--- src/main/java/com/joaocigana/springproject/resources/UserResource.java
+++ src/main/java/com/joaocigana/springproject/resources/UserResource.java
@@ -5,11 +5,10 @@ import com.joaocigana.springproject.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -29,6 +28,15 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id){
     User obj = service.findById(id);
     return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user){
+        user = service.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(user.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(user);
     }
 
 }
