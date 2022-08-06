7923eec8866f603ffbddd343d72864280ef9117e
joao-cigana <joaovitorcigana@gmail.com>
2022-08-06 15:10:33 -0300

    User update

--- src/main/java/com/joaocigana/springproject/resources/UserResource.java
+++ src/main/java/com/joaocigana/springproject/resources/UserResource.java
@@ -45,4 +45,10 @@ public class UserResource {
         return ResponseEntity.noContent().build();
     }
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user){
+        user = service.update(id, user);
+        return ResponseEntity.ok().body(user);
+    }
+
 }
